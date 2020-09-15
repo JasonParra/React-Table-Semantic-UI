94b233473e379602586c732ad18e337fcd81474a
Jason <yayr8@hotmail.com>
2020-09-14 21:02:04 -0400

    cell wrap text fixed

--- src/components/semantic-ui-table/index.js
+++ src/components/semantic-ui-table/index.js
@@ -1,207 +1,213 @@
-import React, { PureComponent } from 'react'
-import { Icon, Menu, Table } from 'semantic-ui-react'
+import React, { PureComponent } from "react";
+import { Icon, Menu, Table } from "semantic-ui-react";
 
-import Styles from './style.module.css'
+import Styles from "./style.module.css";
 
 class CustomTable extends PureComponent {
+	state = {
+		page: 0,
+		pageSelection: null,
+		searchQuery: "",
+		header: "",
+	};
 
-  state = {
-    page: 0,
-    pageSelection: null,
-    searchQuery: "",
-    header: ""
-  }
+	componentDidMount = () => {
+		const { defaultPages } = this.props;
+		this.setState({
+			pageSelection: [0, defaultPages],
+		});
+	};
 
-  componentDidMount = () => {
-    const { defaultPages } = this.props
-    this.setState({
-      pageSelection: [0, defaultPages]
-    })
-  }
+	componentDidUpdate = () => {
+		const { searchQuery, defaultPages } = this.props;
+		if (searchQuery) {
+			if (this.state.searchQuery !== this.props.searchQuery)
+				this.setState({
+					searchQuery,
+					pageSelection: [0, defaultPages],
+					page: 0,
+				});
+		}
+	};
 
-  componentDidUpdate = () => {
-    const { searchQuery, defaultPages } = this.props
-    if (searchQuery) {
-      if (this.state.searchQuery !== this.props.searchQuery)
-        this.setState({
-          searchQuery,
-          pageSelection: [0, defaultPages],
-          page: 0,
-        })
-    }
-  }
+	dynamicsort = (property, order) => {
+		var sort_order = 1;
+		if (order === "desc") {
+			sort_order = -1;
+		}
+		return function (a, b) {
+			if (a[property] < b[property]) {
+				return -1 * sort_order;
+			} else if (a[property] > b[property]) {
+				return 1 * sort_order;
+			} else {
+				return 0 * sort_order;
+			}
+		};
+	};
 
-  dynamicsort = (property, order) => {
-    var sort_order = 1;
-    if (order === "desc") {
-      sort_order = -1;
-    }
-    return function (a, b) {
-      if (a[property] < b[property]) {
-        return -1 * sort_order;
-      } else if (a[property] > b[property]) {
-        return 1 * sort_order;
-      } else {
-        return 0 * sort_order;
-      }
-    }
-  }
+	handleSort = (header) => {
+		const { defaultPages } = this.props;
+		this.setState({
+			header,
+			[header]: this.state[header] === "asc" ? "desc" : "asc",
+			pageSelection: [0, defaultPages],
+			page: 0,
+		});
+	};
 
+	getSort = (header, data) => {
+		const { labels, headers } = this.props;
 
-  handleSort = (header) => {
-    const { defaultPages } = this.props
-    this.setState({
-      header,
-      [header]: this.state[header] === 'asc' ? 'desc' : 'asc',
-      pageSelection: [0, defaultPages],
-      page: 0
-    })
-  }
+		const headerIndex = headers.indexOf(header);
+		const label = labels[headerIndex];
 
+		return data.sort(this.dynamicsort(label, this.state[header] === "asc" ? "desc" : "asc"));
+	};
 
-  getSort = (header, data) => {
-    const { labels, headers } = this.props
+	searchOnData = (query, data) => {
+		const filtered = data.filter((item) => {
+			let obj = {};
+			for (let key of Object.keys(item)) {
+				obj[key] = item[key];
+			}
+			for (let key of Object.keys(obj)) {
+				try {
+					let value = obj[key];
+					let re = new RegExp("W*(" + query + ")W*");
+					if (re.test(value.toString().toLowerCase())) {
+						return true;
+					} else if (re.test(value)) {
+						return true;
+					}
+				} catch (e) {
+					return false;
+				}
+			}
+		});
+		return filtered;
+	};
 
-    const headerIndex = headers.indexOf(header)
-    const label = labels[headerIndex]
+	renderHeaders = () => {
+		const { headers } = this.props;
+		return (
+			<Table.Header>
+				<Table.Row>
+					{(headers || []).map((item, index) => (
+						<Table.HeaderCell key={index} onClick={() => this.handleSort(item)}>
+							<div className={Styles.header}>
+								<div>{item}</div>
+								<div className={Styles.icon}>
+									<Icon name={this.state[item] === "asc" ? "caret up" : "caret down"}></Icon>
+								</div>
+							</div>
+						</Table.HeaderCell>
+					))}
+				</Table.Row>
+			</Table.Header>
+		);
+	};
 
-    return data.sort(this.dynamicsort(label, this.state[header] === 'asc' ? 'desc' : 'asc'))
-  }
+	handleFooter = (i) => {
+		const { defaultPages, data, searchQuery } = this.props;
+		const pagesQ = Math.ceil(
+			(searchQuery ? this.searchOnData(searchQuery, data) : data).length / defaultPages
+		);
 
-  searchOnData = (query, data) => {
-    const filtered = data.filter(item => {
-      let obj = {};
-      for (let key of Object.keys(item)) {
-        obj[key] = item[key];
-      }
-      for (let key of Object.keys(obj)) {
-        try {
-          let value = obj[key];
-          let re = new RegExp("W*(" + query + ")W*");
-          if (re.test(value.toString().toLowerCase())) {
-            return true;
-          } else if (re.test(value)) {
-            return true;
-          }
-        } catch (e) {
-          return false
-        }
+		if (i >= 0 && i < pagesQ)
+			this.setState({
+				pageSelection:
+					i === 0 ? [0, defaultPages] : [i * defaultPages, i * defaultPages + defaultPages],
+				page: i,
+			});
+	};
 
-      }
-    })
-    return filtered
-  }
+	getData = () => {
+		const { data, searchQuery, defaultPages } = this.props;
+		const { pageSelection, header } = this.state;
 
-  renderHeaders = () => {
-    const { headers } = this.props
-    return <Table.Header >
-      <Table.Row>
-        {(headers || []).map(item =>
-          <Table.HeaderCell onClick={() => this.handleSort(item)}>
-            <div className={Styles.header}>
-              <div>
-                {item}
-              </div>
-              <div className={Styles.icon}>
-                <Icon name={this.state[item] === 'asc' ? "caret up" : "caret down"}></Icon>
-              </div>
-            </div>
-          </Table.HeaderCell>)
-        }
-      </Table.Row>
-    </Table.Header >
-  }
+		let _data = data || [];
 
-  handleFooter = (i) => {
-    const { defaultPages, data, searchQuery } = this.props
-    const pagesQ = Math.ceil((searchQuery ? this.searchOnData(searchQuery, data) : data).length / (defaultPages))
+		if (searchQuery) _data = this.searchOnData(searchQuery, _data);
 
-    if (i >= 0 && i < pagesQ)
-      this.setState({
-        pageSelection: i === 0 ? [0, defaultPages]
-          : [i * defaultPages, ((i * defaultPages) + defaultPages)],
-        page: i
-      })
-  }
+		if (header) _data = this.getSort(header, _data);
 
-  getData = () => {
-    const { data, searchQuery, defaultPages } = this.props
-    const { pageSelection, header } = this.state
+		if (pageSelection) _data = _data.slice(...pageSelection);
 
-    let _data = data
+		return _data;
+	};
 
-    if (searchQuery)
-      _data = this.searchOnData(searchQuery, _data)
+	renderData = () => {
+		const { labels } = this.props;
+		const _data = this.getData();
 
-    if (header)
-      _data = this.getSort(header, _data)
+		return (
+			<Table.Body>
+				{(_data || []).map((item, index) => (
+					<Table.Row key={index}>
+						{(labels || []).map((label, index) => (
+							<Table.Cell key={index}>
+								<div className={Styles.cell}>{item[label] || ""}</div>
+							</Table.Cell>
+						))}
+					</Table.Row>
+				))}
+			</Table.Body>
+		);
+	};
 
-    if (pageSelection)
-      _data = _data.slice(...pageSelection)
+	renderFotter = () => {
+		const { defaultPages, labels, data, searchQuery } = this.props;
+		const { page } = this.state;
+		const pagesQ = Math.ceil(
+			(searchQuery ? this.searchOnData(searchQuery, data) : data).length / (defaultPages || 10)
+		);
 
-    return _data
+		let pages = [];
 
-  }
+		for (let i = 1; i <= pagesQ; i++) {
+			pages = [...pages, i];
+		}
 
-  renderData = () => {
-    const { labels } = this.props
-    const _data = this.getData()
+		return (
+			<Table.Footer>
+				<Table.Row>
+					<Table.HeaderCell colSpan={labels.length}>
+						<Menu floated="right" pagination>
+							<Menu.Item as="a" icon onClick={() => this.handleFooter(page - 1)}>
+								<Icon name="chevron left" />
+							</Menu.Item>
+							{pages.map((item, index) => {
+								return (
+									<Menu.Item
+										key={index}
+										style={item - 1 === page ? { backgroundColor: "#E5E5E5" } : {}}
+										as="a"
+										onClick={() => this.handleFooter(item - 1)}
+									>
+										{item}
+									</Menu.Item>
+								);
+							})}
+							<Menu.Item as="a" icon onClick={() => this.handleFooter(page + 1)}>
+								<Icon name="chevron right" />
+							</Menu.Item>
+						</Menu>
+					</Table.HeaderCell>
+				</Table.Row>
+			</Table.Footer>
+		);
+	};
 
-    return <Table.Body>
-      {(_data || []).map(item => <Table.Row>
-        {(labels || []).map(label => <Table.Cell>{item[label] || ""}</Table.Cell>)}
-      </Table.Row>)
-      }
-    </Table.Body>
-  }
-
-
-
-
-  renderFotter = () => {
-
-    const { defaultPages, labels, data, searchQuery } = this.props
-    const { page } = this.state
-    const pagesQ = Math.ceil((searchQuery ? this.searchOnData(searchQuery, data) : data).length / (defaultPages || 10))
-
-    let pages = []
-
-    for (let i = 1; i <= pagesQ; i++) {
-      pages = [...pages, i]
-    }
-
-    return <Table.Footer>
-      <Table.Row>
-        <Table.HeaderCell colSpan={labels.length}>
-          <Menu floated='right' pagination>
-            <Menu.Item as='a' icon onClick={() => this.handleFooter(page - 1)}>
-              <Icon name='chevron left' />
-            </Menu.Item>
-            {pages.map(item => {
-              return <Menu.Item
-                style={item - 1 === page ? { backgroundColor: '#E5E5E5' } : {}}
-                as='a'
-                onClick={() => this.handleFooter(item - 1)}
-              >
-                {item}
-              </Menu.Item>
-            })}
-            <Menu.Item as='a' icon onClick={() => this.handleFooter(page + 1)}>
-              <Icon name='chevron right' />
-            </Menu.Item>
-          </Menu>
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Footer>
-  }
-
-  render() {
-    return <Table celled>
-      {this.renderHeaders()}
-      {this.renderData()}
-      {this.renderFotter()}
-    </Table>
-  }
+	render() {
+		return (
+			<Table celled>
+				{this.renderHeaders()}
+				{this.renderData()}
+				{this.renderFotter()}
+			</Table>
+		);
+	}
 }
 
-export default CustomTable
+export default CustomTable;
